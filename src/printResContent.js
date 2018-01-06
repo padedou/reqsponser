39e372404b51034dfe9d632907791cea54735f3f
padedou <padedou@gmail.com>
2018-01-06 12:22:31 +0200

    Changes the content type checking method.
    Sometimes Content-Type header may contain additional info.
    e.g.: aplication/json; charset=utf-8

--- src/printResContent.js
+++ src/printResContent.js
@@ -1,11 +1,9 @@
 export default function (response, div) {
-  switch (response.headers.get('Content-Type')) {
-    case 'application/json':
-      response.json().then((data) => {
-        div.innerHTML = JSON.stringify(data)
-      })
-      break
-    default:
-      console.log('Could not read the "Content-Type" header')
+  const contentType = response.headers.get('Content-Type')
+
+  if (contentType.includes('application/json')) {
+    response.json().then((data) => {
+      div.innerHTML = JSON.stringify(data)
+    })
   }
 }
